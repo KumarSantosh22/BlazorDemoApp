a2fbc4cec4cd8b890b4ee03436a34d53246d8669
Santhosh.Kumar <santosh.k@technovert.com>
2023-08-17 15:14:27 +0530

    initial commit

---
+++ wwwroot/js/interop.js
@@ -0,0 +1,15 @@
+﻿function showAlert() {
+    alert("JS interop demo");
+}
+
+function toggleBackground(isWhilte) {
+    let testInterop = document.getElementById("test-interop");
+    if (isWhilte) {
+        testInterop.classList.remove('bg-secondary');
+        testInterop.classList.add('bg-success');
+    }
+    else {
+        testInterop.classList.remove('bg-success');
+        testInterop.classList.add('bg-secondary');
+    }
+}
